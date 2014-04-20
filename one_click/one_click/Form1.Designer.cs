1e93b3be1f8641d620c8f752f0b7a8061f8b0314
Андрей Саган <shelljke@gmail.com>
2014-04-20 21:17:20 +0600

    Программа для обработки изображений в 1 клик. РЕАЛИЗОВАНО: -интерфейс(частично) -три фильтра: инверсия, резкость, размытие -сохранение файла -оптимизация обработки больших фалов(в смысле разрешения): изображение, размер которого превышает размер окна, изменяет разрешение при открытии, это позволяет сократить время обработки и быстрей видеть результат в окне просмотра. при сохранении изображения на жесткий диск оно обрабатывается уже в полном размере -для каждого фильтра добавлен эскиз, представляющий собой миниатюру изображения с примененным к ней фильтром
    ПЛАНЫ:
    -добавить фильтры
    -доработать/переработать интерфейс
    -сделать интенсивность для некоторых фильтров(размытие, резкость, эрозия и т.д.)
    -решить вопрос с обработкой проблемных мест фотографий: крайние полосы изображения шириной в половину матрицы свертки
    
    ОСОБЕННОСТИ (НЕ БАГ, А ФИЧА):
    -нельзя применять фильтры поверх друг друга: для этого нужно сначала сохранить обработанное изображение, а затем открыть его и применить другой фильтр. возможно, будет изменено в будущем

---
+++ one_click/one_click/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace one_click
+{
+    partial class mainwindow
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hi = new System.Windows.Forms.Label();
+            this.begin = new System.Windows.Forms.Label();
+            this.beginpic = new System.Windows.Forms.PictureBox();
+            this.open = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.picture = new System.Windows.Forms.PictureBox();
+            this.inv = new System.Windows.Forms.PictureBox();
+            this.shrp = new System.Windows.Forms.PictureBox();
+            this.blr = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.beginpic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inv)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.shrp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blr)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // hi
+            // 
+            this.hi.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F);
+            this.hi.Location = new System.Drawing.Point(0, 22);
+            this.hi.Name = "hi";
+            this.hi.Size = new System.Drawing.Size(683, 118);
+            this.hi.TabIndex = 1;
+            this.hi.Text = "Приветствую в программе One Click Image Editor! Для продолжения просто нажмите \"Н" +
+    "ачать\", а затем выберите изображение, которое хотите обработать.";
+            this.hi.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.hi.Click += new System.EventHandler(this.hi_Click);
+            // 
+            // begin
+            // 
+            this.begin.AutoSize = true;
+            this.begin.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F);
+            this.begin.Location = new System.Drawing.Point(300, 276);
+            this.begin.Name = "begin";
+            this.begin.Size = new System.Drawing.Size(99, 29);
+            this.begin.TabIndex = 2;
+            this.begin.Text = "Начать!";
+            this.begin.Click += new System.EventHandler(this.begin_Click);
+            // 
+            // beginpic
+            // 
+            this.beginpic.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.beginpic.Image = global::one_click.Properties.Resources.begin;
+            this.beginpic.Location = new System.Drawing.Point(225, 164);
+            this.beginpic.Name = "beginpic";
+            this.beginpic.Size = new System.Drawing.Size(250, 250);
+            this.beginpic.TabIndex = 0;
+            this.beginpic.TabStop = false;
+            this.beginpic.Click += new System.EventHandler(this.beginpic_Click_1);
+            // 
+            // open
+            // 
+            this.open.Location = new System.Drawing.Point(12, 12);
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(243, 23);
+            this.open.TabIndex = 3;
+            this.open.Text = "Открыть";
+            this.open.UseVisualStyleBackColor = true;
+            this.open.Visible = false;
+            this.open.Click += new System.EventHandler(this.open_Click);
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(411, 12);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(261, 23);
+            this.save.TabIndex = 4;
+            this.save.Text = "Сохранить";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Visible = false;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // picture
+            // 
+            this.picture.BackColor = System.Drawing.Color.Transparent;
+            this.picture.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.picture.Location = new System.Drawing.Point(-1, -1);
+            this.picture.Name = "picture";
+            this.picture.Size = new System.Drawing.Size(684, 539);
+            this.picture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picture.TabIndex = 5;
+            this.picture.TabStop = false;
+            this.picture.Visible = false;
+            // 
+            // inv
+            // 
+            this.inv.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.inv.Location = new System.Drawing.Point(5, 475);
+            this.inv.Name = "inv";
+            this.inv.Size = new System.Drawing.Size(88, 50);
+            this.inv.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.inv.TabIndex = 6;
+            this.inv.TabStop = false;
+            this.inv.WaitOnLoad = true;
+            this.inv.Click += new System.EventHandler(this.inv_Click);
+            // 
+            // shrp
+            // 
+            this.shrp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.shrp.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.shrp.Location = new System.Drawing.Point(99, 475);
+            this.shrp.Name = "shrp";
+            this.shrp.Size = new System.Drawing.Size(88, 50);
+            this.shrp.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.shrp.TabIndex = 7;
+            this.shrp.TabStop = false;
+            this.shrp.WaitOnLoad = true;
+            this.shrp.Click += new System.EventHandler(this.shrp_Click);
+            // 
+            // blr
+            // 
+            this.blr.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.blr.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.blr.Location = new System.Drawing.Point(193, 475);
+            this.blr.Name = "blr";
+            this.blr.Size = new System.Drawing.Size(88, 50);
+            this.blr.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.blr.TabIndex = 8;
+            this.blr.TabStop = false;
+            this.blr.Click += new System.EventHandler(this.blur_Click);
+            // 
+            // mainwindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(684, 537);
+            this.Controls.Add(this.blr);
+            this.Controls.Add(this.shrp);
+            this.Controls.Add(this.inv);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.open);
+            this.Controls.Add(this.begin);
+            this.Controls.Add(this.beginpic);
+            this.Controls.Add(this.picture);
+            this.Controls.Add(this.hi);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "mainwindow";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.beginpic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inv)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.shrp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.blr)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox beginpic;
+        private System.Windows.Forms.Label hi;
+        private System.Windows.Forms.Label begin;
+        private System.Windows.Forms.Button open;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.PictureBox picture;
+        private System.Windows.Forms.PictureBox inv;
+        private System.Windows.Forms.PictureBox shrp;
+        private System.Windows.Forms.PictureBox blr;
+    }
+}
+
---
+++ one_click/one_click/Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace one_click
+{
+    public partial class mainwindow : Form
+    {
+        public byte div, offset;
+        public int kernel_w, kernel_h;
+        public int[,] kernel;
+        public mainwindow()
+        {
+            InitializeComponent();
+        }
+
+        private void hi_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void beginpic_Click_1(object sender, EventArgs e)
+        {
+            hi.Hide();
+            begin.Hide();
+            beginpic.Hide();
+ 
+            open.Show();
+            save.Show();
+            picture.Show();
+        }
+        public Bitmap origin_size_image, image_l, image_l1;
+        public float w, h;
+        public int a;
+        private void open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog() { Filter = "Файл изображения|*.jpg" };
+            var result = op.ShowDialog(this);
+            if (result == DialogResult.OK)
+            {
+                origin_size_image = (Bitmap)Image.FromFile(op.FileName);
+                w = origin_size_image.Width;
+                h = origin_size_image.Height;
+
+                if (origin_size_image.Width >= 88 && origin_size_image.Width >= origin_size_image.Height)
+                {
+                    float k = (float)origin_size_image.Width / 88;
+                    w = 88;
+                    h = origin_size_image.Height / k;
+                }
+                else if (origin_size_image.Height >= 50 && origin_size_image.Width < origin_size_image.Height)
+                {
+                    float k = (float)origin_size_image.Height / 50;
+                    h = 50;
+                    w = origin_size_image.Width / k;
+                }
+
+
+                Size size_f = new Size((int)w, (int)h);
+
+                image_l = new Bitmap(origin_size_image, size_f);
+               // image_l1 = new Bitmap(origin_size_image, size_f);
+                inversion();
+                kern(image_l);
+                inv.Image = image_l;
+
+                image_l = new Bitmap(origin_size_image, size_f);
+                image_l1 = new Bitmap(origin_size_image, size_f);
+                sharpen();
+                kern(image_l);
+                shrp.Image = image_l;
+
+                image_l = new Bitmap(origin_size_image, size_f);
+                image_l1 = new Bitmap(origin_size_image, size_f);
+                blur();
+                kern(image_l);
+                blr.Image = image_l;
+
+                w = origin_size_image.Width;
+                h = origin_size_image.Height;
+
+                if (origin_size_image.Width >= 670 && origin_size_image.Width >= origin_size_image.Height)
+                {
+                    float k = (float)origin_size_image.Width / 670;
+                    w = 670;
+                    h = origin_size_image.Height / k;
+                }else if (origin_size_image.Height >= 430 && origin_size_image.Width < origin_size_image.Height)
+                {
+                    float k =(float) origin_size_image.Height / 430;
+                    h = 430;
+                    w = origin_size_image.Width / k;
+                }
+
+                Size size = new Size((int)w, (int)h);
+                image_l = new Bitmap(origin_size_image, size);
+             //   image_l1 = new Bitmap(origin_size_image, size);
+              
+                picture.Image = image_l;
+
+                this.BackColor = Color.Black;
+            }
+        }
+        private void begin_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void inv_Click(object sender, EventArgs e)
+        {
+            Size size = new Size((int)w, (int)h);
+            image_l = new Bitmap(origin_size_image, size);
+            inversion();
+            kern(image_l);
+            picture.Image = image_l;
+            a = 1;
+        }
+
+        private void shrp_Click(object sender, EventArgs e)
+        {
+            Size size = new Size((int)w, (int)h);
+            image_l = new Bitmap(origin_size_image, size);
+            sharpen();
+            kern(image_l);
+            picture.Image = image_l;
+            a = 2;
+        }
+
+        private void blur_Click(object sender, EventArgs e)
+        {
+            Size size = new Size((int)w, (int)h);
+            image_l = new Bitmap(origin_size_image, size);
+            blur();
+            kern(image_l);
+            picture.Image = image_l;
+            a = 3;
+        }
+
+        void inversion()
+        {
+            kernel = new int[1, 1];
+
+            kernel[0, 0] = -1;
+
+            div = 1;
+            offset = 255;
+            kernel_h = kernel_w = 1;
+        }
+
+        void sharpen()
+        {
+            kernel = new int[3, 3];
+
+            kernel[0, 0] = -1;
+            kernel[1, 0] = -1;
+            kernel[2, 0] = -1;
+
+            kernel[0, 1] = -1;
+            kernel[1, 1] = 9;
+            kernel[2, 1] = -1;
+
+            kernel[0, 2] = -1;
+            kernel[1, 2] = -1;
+            kernel[2, 2] = -1;
+
+            div = 1;
+            offset = 0;
+            kernel_h = kernel_w = 3;           
+        }
+
+        void blur()
+        {
+            kernel = new int[3, 3];
+
+            kernel[0, 0] = 1;
+            kernel[1, 0] = 1;
+            kernel[2, 0] = 1;
+
+            kernel[0, 1] = 1;
+            kernel[1, 1] = 1;
+            kernel[2, 1] = 1;
+
+            kernel[0, 2] = 1;
+            kernel[1, 2] = 1;
+            kernel[2, 2] = 1;
+
+            div = 9;
+            offset = 0;
+            kernel_h = kernel_w = 3;
+        }
+
+
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            var sfd = new SaveFileDialog();
+            sfd.Filter = "Файл изображения|*.jpg";
+            image_l = new Bitmap(origin_size_image);
+
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+
+                var fileName = sfd.FileName;
+                var fileStream = File.Create(sfd.FileName);
+
+              if (a == 1)
+                {
+                    inversion();
+                    kern(image_l);
+                    picture.Image = image_l;
+                } else if (a == 2)
+                {
+                    sharpen();
+                    kern(image_l);
+                } else if (a == 3)
+                {
+                    blur();
+                    kern(image_l);
+                }
+               image_l.Save(fileStream, origin_size_image.RawFormat);
+               fileStream.Close();
+            }
+        }
+
+        Bitmap kern(Bitmap image_l)
+        {
+            image_l1 = (Bitmap)image_l.Clone();
+            int rSum = 0, gSum = 0, bSum = 0;
+            var color = new Color();
+            for (int x = kernel_w / 2; x < image_l.Width - kernel_w / 2; x++)
+            {
+                for (int y = kernel_w / 2; y < image_l.Height - kernel_w / 2; y++)
+                {
+                    for (int j = 0; j < kernel_w; j++)
+                    {
+                        for (int i = 0; i < kernel_h; i++)
+                        {
+                            color = image_l1.GetPixel(x - kernel_h / 2 + i, y - kernel_w / 2 + j);
+
+                            byte r = color.R;
+                            byte g = color.G;
+                            byte b = color.B;
+                            int a = kernel[i, j];
+
+                            rSum += r * a;
+                            gSum += g * a;
+                            bSum += b * a;
+                        }
+                    }
+                    rSum /= div;
+                    gSum /= div;
+                    bSum /= div;
+
+                    rSum += offset;
+                    gSum += offset;
+                    bSum += offset;
+
+                    if (rSum > 255) rSum = 255;
+                    if (rSum < 0) rSum = 0;
+
+                    if (gSum > 255) gSum = 255;
+                    if (gSum < 0) gSum = 0;
+
+                    if (bSum > 255) bSum = 255;
+                    if (bSum < 0) bSum = 0;
+
+                    image_l.SetPixel(x, y, Color.FromArgb((byte)rSum, (byte)gSum, (byte)bSum));
+                    rSum = gSum = bSum = 0;
+                }
+            }
+
+            return image_l;
+        }
+    }
+
+}
+
